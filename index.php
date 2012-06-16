126c329e062bd014b84f66264a0d502242c56e73
Pierre-Alexandre Meyer <pierre@ning.com>
2012-06-16 16:08:22 -0700

    Add index.php
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Copyright 2011-2012 Ning, Inc.
+ *
+ * Ning licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+include_once(dirname(__FILE__) . '/includes/header.php');
+include_once(dirname(__FILE__) . '/includes/nav.php');
+
+?>
+<div class="container">
+    <div class="hero-unit">
+        <h1>Welcome to CloudKilling!</h1>
+
+        <p>CloudKilling is an awesome imaginary cloud provider that uses <a href="https://github.com/killbilling" target="_blank">Killbill</a>
+            for its subscription needs.</p>
+
+        <p><a class="btn btn-primary btn-large" href="account.php">Create an account today &raquo;</a></p>
+    </div>
+</div>
+<?php
+include_once(dirname(__FILE__) . '/includes/footer.php');
+?>
